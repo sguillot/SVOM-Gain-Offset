9ff8011f4df4f142118e966f95e339da185fd5e8
Sebastien Guillot <sebguillot60@gmail.com>
2022-06-16 19:42:00 +0200

    ConfigParser implemented
    instead of ParamReadar

--- eclairs_gain-offset.py
+++ eclairs_gain-offset.py
@@ -1,6 +1,6 @@
 import os
 import time
-import argparse
+import argparse, configparser
 import numpy as np
 import matplotlib.pyplot as plt
 import tqdm
@@ -24,6 +24,7 @@ from svom.io_utils import write_rel
 # from svom.utils import Ch2En
 import svom.plot_utils as plot_utils
 from svom.fit_utils import FittingEngine
+from svom.file_reader import FileReader as ParamReader
 
 
 desc = """
@@ -61,9 +62,10 @@ def range_limited_int(arg):
         return f
 
 parser = argparse.ArgumentParser(description=desc)
+parser.add_argument("-c", "--config_file", type=str, help='Config file')
 # TODO:  THESE ARGUMENTS SHOULD PROBABLY BE MADE MANDATORY
 parser.add_argument("--spectra", help="Input spectra (fits file matrix of 6400 spectra)", type=str, default=None)
-parser.add_argument("--matrix", help="Input Gain-Offset matrix (fits file)", type=str, default="AUX-ECL-PIX-CAL-20170101Q01.fits")
+parser.add_argument("--matrix", help="Input Gain-Offset matrix (fits file)", type=str, default=None)
 parser.add_argument("--lines", help="Input spectral lines to fit in spectra (ascii file...for now)", type=str, default="lines_keV_4blocks.txt")
 parser.add_argument("--rootname", help="Outputs rootname", type=str, default="Default")
 # OPTIONAL ARGUMENTS
@@ -73,10 +75,21 @@ parser.add_argument("--nbpix", help="Number of pixels to run (default=None, for 
 parser.add_argument("--pixels", help="One or more specific pixels to run ([1,6400], default=None, ignored if --nbpix is set)", nargs='+', type=range_limited_int, default=None)
 parser.add_argument("--plots", help="Makes initial and final plots (default=False)", default=False, action='store_true')
 parser.add_argument("--plotall", help="Makes all plot along the way, one per pixel (default=False)", default=False, action='store_true')
-parser.add_argument("--showrawspec", help="Shows the raw spectrum (no Energy redistribution", default=False, action='store_true')
+parser.add_argument("--showrawspec", help="Shows the raw spectrum (no Energy redistribution)", default=False, action='store_true')
 parser.add_argument("--width", help="width (in keV) for energy redistribution", type=float, default=1.1)
 args = parser.parse_args()
 
+if args.config_file:
+    config = configparser.ConfigParser()
+    config.read(args.config_file)
+    defaults = {}
+    defaults.update(dict(config.items("Defaults")))
+    for arg in vars(args):
+        if type(getattr(args, arg)) is bool:
+            if arg in defaults:
+                defaults[arg] = config.getboolean('Defaults', arg)
+    parser.set_defaults(**defaults)
+    args = parser.parse_args()        # Overwrite arguments from the parser
 
 # DEPRECATED - plotall now works in Pool
 # if (args.plotall is True) and (args.proc > 1):
